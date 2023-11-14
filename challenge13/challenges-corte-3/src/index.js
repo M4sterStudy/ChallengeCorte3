9599222c93d68747ffc3e95c3e1b980398ee7180
M4sterStudy <123605030+M4sterStudy@users.noreply.github.com>
2023-11-14 12:46:01 -0500

    cambios

--- challenge13/challenges-corte-3/src/index.js
+++
@@ -1,7 +0,0 @@
-import { createRoot } from "react-dom";
-import App from "./App.jsx";
-
-const rootElement = document.getElementById("root");
-const root = createRoot(rootElement);
-
-root.render(<App />);
